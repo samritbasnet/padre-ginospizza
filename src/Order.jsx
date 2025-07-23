2eaea9b93df51a84758ad54cda34ff1745f1aaaf
Samrit Basnet <samritbasnet70@gmail.com>
2025-07-23 09:01:46 -0400

    added useContext

--- src/Order.jsx
+++ src/Order.jsx
@@ -1,7 +1,7 @@
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import Cart from "./Cart";
 import Pizza from "./Pizza";
-
+import { CartContext } from "./contexts";
 // feel free to change en-US / USD to your locale
 const intl = new Intl.NumberFormat("en-US", {
   style: "currency",
@@ -12,8 +12,8 @@ export default function Order() {
   const [pizzaType, setPizzaType] = useState("pepperoni");
   const [pizzaSize, setPizzaSize] = useState("M");
   const [pizzaTypes, setPizzaTypes] = useState([]);
+  const [cart, setCart] = useContext(CartContext);
   const [loading, setLoading] = useState(true);
-  const [cart, setCart] = useState([]);
 
   async function checkout() {
     setLoading(true);
