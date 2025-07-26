75171630f29806961f7560e0296b2010427e8ff6
Samrit Basnet <samritbasnet70@gmail.com>
2025-07-26 18:55:46 -0400

    models added for pastorder

--- src/api/getPastOrder.js
+++ src/api/getPastOrder.js
@@ -1,5 +1,6 @@
-export default async function getPastOrders(page) {
-  const response = await fetch(`/api/past-orders?page=${page}`);
+export default async function getPastOrders(order) {
+  await new Promise(resolve => setTimeout(resolve, 10000));
+  const response = await fetch(`/api/past-order/${order}`);
   const data = await response.json();
   return data;
 }
